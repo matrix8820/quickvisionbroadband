b507abf495113363acac5d009c3a50513d484d30
matrix8820 <ahonaeservices@gmail.com>
2026-07-06 20:54:57 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,43 @@
+// Quick Vision Hero Slider
 
+const slides = [
+
+"images/banner1.jpg",
+
+"images/banner2.jpg"
+
+];
+
+let current = 0;
+
+function changeSlide(){
+
+const hero = document.querySelector(".hero");
+
+if(hero){
+
+hero.style.backgroundImage =
+
+`url(${slides[current]})`;
+
+hero.style.backgroundSize = "cover";
+
+hero.style.backgroundPosition = "center";
+
+hero.style.transition = "all 1s ease";
+
+current++;
+
+if(current >= slides.length){
+
+current = 0;
+
+}
+
+}
+
+}
+
+setInterval(changeSlide,4000);
+
+changeSlide();
